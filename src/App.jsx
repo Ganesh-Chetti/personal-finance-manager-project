84054fd2af103948c2fe5107bbbdfe1704fe2531
Ganesh <ganeshpatelchetti@gmail.com>
2024-12-03 12:38:54 +0530

    react

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import Signup from "./pages/signup.jsx";
+import Signup from "./pages/signup.jsx"
 import Login from "./pages/login.jsx";
 import Home from "./pages/home.jsx";
 import Expenses from "./pages/expenses.jsx";
