f9807116a22f1d17b94fb0497300f2e713545f8d
Ganesh <ganeshpatelchetti@gmail.com>
2024-12-04 17:20:52 +0530

    source file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import Signup from "./Pages/signup.jsx"
-import Login from "./Pages/login.jsx";
-import Home from "./Pages/home.jsx";
-import Expenses from "./Pages/expenses.jsx";
-import Summary from "./Pages/summary.jsx";
+import Signup from "./Pages/Signup.jsx";
+import Login from "./Pages/Login.jsx";
+import Home from "./Pages/Home.jsx";
+import Expenses from "./Pages/Expenses.jsx";
+import Summary from "./Pages/Summary.jsx";
 //git 
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem("currentUser"));
--- src/pages/Summary.jsx
+++ src/pages/Summary.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import Navbar from "../components/navbar.jsx";
+import Navbar from "../components/Navbar.jsx";
 import '../styles/summary.css'; // Import the custom CSS
 
 const Summary = () => {
